830464ea768412d993bb0b3b5774ca407b96cdf1
wangluyuan <545000692@qq.com>
2017-03-22 16:42:42 +0800

    添加ajax请求

--- build/dev-server.js
+++ build/dev-server.js
@@ -14,7 +14,14 @@ var server = new WebpackDevServer(compiler, {
     },
     publicPath: devConfig.config.output.publicPath,
     inline: true,
-    hot: true
+    hot: true,
+    // 请求代理设置
+    proxy: {
+        '/api': {
+            target: 'http://localhost:3000',
+            pathRewrite: {'^/api': '/api'}
+        }
+    }
 })
 server.listen(devConfig.port, function (err) {
     if (err) {
